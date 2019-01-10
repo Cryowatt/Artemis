98fdfb641b9d9de7ae1b933e781954858f4e7ed6
Eric Carter <eric.carter@iqvia.com>
2019-01-10 15:59:07 -0800

    Some stuff

---
+++ Artemis.QueryNode.Application/Controllers/QueryController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.OData;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Artemis.QueryNode.Application.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class QueryController : ODataController
+    {
+        public class Foo { public string Name { get; set; } }
+        
+        public List<Foo> Get()
+        {
+            return new List<Foo> { new Foo { Name = "value1" } };
+        }
+    }
+}
